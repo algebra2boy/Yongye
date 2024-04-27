33dce9256b8b4b6fc4da02840e5eaeb053ec263b
Yongye Tan <yongyetan1209@gmail.com>
2024-04-26 23:43:26 -0400

    remove previous code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,13 @@
 <script>
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
-	import './styles.css';
-
-	/** @type {import('./$types').LayoutServerData} */
-	export let data;
-
-	$: if (browser && data?.analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId: data.analyticsId
-		});
-	}
+	import '../app.pcss';
 </script>
 
-<div class="app">
-	<Header />
+<svelte:head>
+	<title>Yongye's website</title>
+</svelte:head>
 
-	<main>
+<div class="flex justify-center min-h-screen bg-lime-50 p-10">
+	<div class="md:w-2/3 w-full overflow-auto p-4 bg-amber-50 shadow-xl rounded-xl">
 		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</div>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>

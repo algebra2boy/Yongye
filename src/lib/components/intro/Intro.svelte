88074a83023b986e8770b7c030371a69f84be9c6
Yongye Tan <yongyetan1209@gmail.com>
2024-04-27 17:26:51 -0400

    fix image size on smaller size

--- src/lib/components/intro/Intro.svelte
+++ src/lib/components/intro/Intro.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex items-center">
-	<img src="/profile.jpeg" alt="Yongye Tan's profile" class="rounded-full w-24 h-24" />
+	<img src="/profile.jpeg" alt="Yongye Tan's profile" class="rounded-full w-24 h-24 md:w-18 md:h-18 sm:w-14 sm:h-14" />
 	<div class="pl-3 pt-1">
 		<h1 class="text-2xl font-thin">Yongye Tan</h1>
 		<div class="flex gap-1.5">

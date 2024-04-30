ada5f8c5a89c8c7a561a269a327117b414948ef4
Yongye Tan <yongyetan1209@gmail.com>
2024-04-30 19:17:44 -0400

    able to open and close all semester folders

--- src/lib/components/project/school/SchoolProject.svelte
+++ src/lib/components/project/school/SchoolProject.svelte
@@ -12,13 +12,38 @@
 		'Fall 2023',
 		'Spring 2024'
 	];
+
+	let isAllSemesterFolderOpen: boolean = true;
+	let isAllFileOpen: boolean = false;
+
+	const openSemesterFolder = () => (isAllSemesterFolderOpen = !isAllSemesterFolderOpen);
+	// const openAllFile = () => {
+	// 	const allfolder = document.querySelectorAll('.folder');
+	// 	allfolder.forEach((folder) => {
+	// 		folder.classList.add('expanded');
+	// 		folder.
+	// 	})
+	// };
+ 
 </script>
 
 <div class="pt-2 pl-1 rounded-lg">
 	<h1 class="text-xl font-extralight">SCHOOL PROJECTS & LOGISTICS</h1>
 	<hr class="divide-solid border-slate-500 mt-1 border-1" />
 
+	<div class="flex flex-row bg-blue-100">
+		<button on:click={openSemesterFolder}>
+			<p>Open root folder</p>
+		</button>
+	</div>
+<!-- 
+	<div class="flex flex-row bg-blue-100">
+		<button on:click={openAllFile}>
+			<p>Open all file</p>
+		</button>
+	</div> -->
+
 	{#each semesters as semester, index}
-		<Folder name={semester} files={schoolProjects[index]} expanded />
+		<Folder name={semester} files={schoolProjects[index]} expanded={isAllSemesterFolderOpen} />
 	{/each}
 </div>

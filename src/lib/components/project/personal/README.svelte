5443e1c05659dd521095934b2b8010cf526c3341
Yongye Tan <yongyetan1209@gmail.com>
2024-05-17 20:20:46 -0400

    more redesign

--- src/lib/components/project/personal/README.svelte
+++ src/lib/components/project/personal/README.svelte
@@ -10,11 +10,12 @@
 
 <!-- https://stackoverflow.com/questions/74914969/override-65ch-max-width-in-tailwind-css-typography -->
 <div
-	class="prose prose-sm prose-stone !max-w-none
+	class="w-full prose prose-sm !max-w-none
 	prose-a:font-medium prose-a:text-gray-800
-	prose-headings:font-serif prose-headings:font-normal
-	prose-code:text-sm prose-pre:rounded-lg prose-pre:!bg-gray-800 prose-pre:px-3 prose-pre:py-3 prose-pre:leading-loose prose-code:before:content-none prose-code:after:content-none before:[&_blockquote_p:first-of-type]:content-none after:[&_blockquote_p:last-of-type]:content-none prose-img:max-h-[80vh]
-	prose-img:mx-auto prose-video:max-h-[80vh] prose-video:mx-auto"
+	prose-headings:font-mono prose-headings:font-normal
+	prose-code:text-sm prose-pre:rounded-lg prose-pre:!bg-gray-800 prose-pre:px-3 prose-pre:py-3 prose-pre:leading-loose prose-code:before:content-none prose-code:after:content-none before:[&_blockquote_p:first-of-type]:content-none after:[&_blockquote_p:last-of-type]:content-none
+	prose-img:max-h-[80vh] prose-img:mx-auto
+	prose-video:max-h-[80vh] prose-video:mx-auto"
 >
 	{@html marked(readme)}
 </div>

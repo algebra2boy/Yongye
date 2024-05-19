631c962a8443e81f8cfd835f8fc68a785e96ebf3
Yongye Tan <yongyetan1209@gmail.com>
2024-05-19 12:32:27 -0400

    remove object scale down for profile image (causign distorted)

--- src/lib/components/intro/Intro.svelte
+++ src/lib/components/intro/Intro.svelte
@@ -2,7 +2,7 @@
 	<img
 		src="/profile.jpeg"
 		alt="Yongye Tan's profile"
-		class="rounded-lg object-scale-down w-20 h-20 md:w-18 md:h-18"
+		class="rounded-lg w-20 h-20 md:w-18 md:h-18"
 	/>
 	<div class="pl-3 pt-1">
 		<p class="text-sm pt-2">

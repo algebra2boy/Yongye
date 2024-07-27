ff4fdff195c9847e8e48ac3ae514291b3387761a
Yongye Tan <yongyetan1209@gmail.com>
2024-07-27 14:55:37 -0400

    Updated

--- src/lib/components/intro/Intro.svelte
+++ src/lib/components/intro/Intro.svelte
@@ -6,9 +6,9 @@
 	/>
 	<div class="pl-3 pt-1">
 		<p class="text-sm pt-2">
-			I am a junior at the University of Massachusetts Amherst, majoring in Computer Science and
+			I am a senior at the University of Massachusetts Amherst, majoring in Computer Science and
 			Mathematics. I am passionate about software engineering, API, iOS dev, full stack and game
-			dev. I'm interning in <span class="text-green-600 font-semibold">Fidelity</span>
+			dev. I'm interning at <span class="text-green-600 font-semibold">Fidelity</span>
 			this summer!
 		</p>
 	</div>

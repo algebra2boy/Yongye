a2b3a7e469742471b6ae7a12116c402bd7b8b2f9
Yongye Tan <yongyetan1209@gmail.com>
2025-01-01 14:10:29 -0500

    remove duplicate post

--- src/lib/components/blog/Blog.svelte
+++ src/lib/components/blog/Blog.svelte
@@ -8,9 +8,3 @@
 	date={new Date(2024, 11, 31)}
 	imageURL={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRG94baP7Te4yVwWpicSAMvz46oIHYHzLo-w&s'}
 />
-<BlogPreview
-	title={'First Blog Post'}
-	subtitle={'Happy New Year - 2025, the year of the blog'}
-	date={new Date(2024, 11, 31)}
-	imageURL={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRG94baP7Te4yVwWpicSAMvz46oIHYHzLo-w&s'}
-/>
